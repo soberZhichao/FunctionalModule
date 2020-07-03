ccedfeb0ef59fed2fc23c17521b4125ba92848d9
诺离 <lizhichao@n22.com.cn>
2020-07-03 10:41:15 +0800

    变基

--- FunctionalModule/FunctionalModule/AppDelegate.swift
+++ FunctionalModule/FunctionalModule/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-//冲突测试
+//这里测试变基
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
