85a426f3e026737bc07b0cc34faaa9765fbe061c
诺离 <lizhichao@n22.com.cn>
2020-07-03 15:25:36 +0800

    develop-6

--- FunctionalModule/FunctionalModule/AppDelegate.swift
+++ FunctionalModule/FunctionalModule/AppDelegate.swift
@@ -11,7 +11,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-//master-5
+//develop-6
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
