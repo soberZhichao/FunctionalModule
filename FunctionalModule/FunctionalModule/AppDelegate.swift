9042e4c6a7fe1e31d4cdde16ad760d72cad0f9ca
诺离 <lizhichao@n22.com.cn>
2020-07-03 10:44:16 +0800

    master 标识

--- FunctionalModule/FunctionalModule/AppDelegate.swift
+++ FunctionalModule/FunctionalModule/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 //master
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
